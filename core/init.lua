1159b1a659329752d0850ddb1a92e0b7292eb45a
Tom Bell <tomb@tomb.io>
2024-09-09 02:23:13 +0100

    Move mana power color override to config addon

--- core/init.lua
+++ core/init.lua
@@ -1,11 +1,5 @@
 local _, L = ...
 
--- Use embedded or global oUF
-local oUF = L.oUF or oUF
-
--- Update mana color to a better color
-oUF.colors.power["MANA"] = { 0, 0.55, 1 }
-
 -- Config table
 L.C = oUF_RiphieConfig
 
